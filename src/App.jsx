8227ab2258b4f8341a57c8eb65aa508b0401411a
ib0222 <ibrahim.ismayilzade221@gmail.com>
2023-08-18 18:48:49 +0400

    build

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import SettingsContext from "./components/SettingsContext";
 
 function App() {
   const [showSettings, setShowSettings] = useState(false);
-  const [workMinutes, setWorkMinutes] = useState(45);
-  const [breakMinutes, setBreakMinutes] = useState(15);
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5);
   const [automatic,setAutomatic] = useState(false);
   return (
     <>
